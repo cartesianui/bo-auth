4f96ba09008886b53f65f22e0a4cc37fd9d6829b
Syed Ali Murtaza <ali@kazmi.me>
2025-10-30 12:29:26 +0500

    Add template in component file

--- src/lib/widgets/roles-lookup/roles-lookup-widget.component.ts
+++ src/lib/widgets/roles-lookup/roles-lookup-widget.component.ts
@@ -1,31 +1,78 @@
-import { Component, Injector, Input, Output, OnInit, EventEmitter } from '@angular/core';
-import { ListingControlsComponent } from '@cartesianui/common';
+import { Component, OnInit, inject, runInInjectionContext, effect } from '@angular/core';
+import { RequestCriteria, RequestCriteriaFactory } from '@cartesianui/core';
+import { LookupFormControlComponent } from '@cartesianui/common';
 import { AuthorizationSandbox } from '../../authorization.sandbox';
 import { Role } from '../../models';
 import { FORM_IMPORTS } from '../../authorization.imports';
 
 @Component({
-    selector: 'roles-lookup',
-    exportAs: 'roles-lookup',
-    templateUrl: './roles-lookup-widget.component.html',
-    imports: [...FORM_IMPORTS],
-    standalone: true
+  selector: 'roles-lookup',
+  exportAs: 'roles-lookup',
+  template: `
+    <div class="row">
+      <div class="card mt-2">
+        <div class="card-header">
+          <strong>Attach Roles</strong>
+        </div>
+        <div class="card-body">
+          <div class="form-group">
+            <lookup-form-control [options]="items()" optionField="name" [multi]="true" [(value)]="value" placeholder="Select roles..." required></lookup-form-control>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+  imports: [...FORM_IMPORTS, LookupFormControlComponent],
+  standalone: true
 })
-export class RolesLookupWidgetComponent extends ListingControlsComponent<Role> implements OnInit {
-  @Input() ignoreOptions: Array<Role> = [];
+export class RolesLookupWidgetComponent extends LookupFormControlComponent<Role> implements OnInit {
+  protected sb = inject(AuthorizationSandbox);
 
-  constructor(
-    injector: Injector,
-    protected sb: AuthorizationSandbox
-  ) {
-    super(injector);
-  }
+  protected criteriaFactory = inject(RequestCriteriaFactory);
+
+  criteria: RequestCriteria;
 
   ngOnInit(): void {
     this.initCriteria().limit(100000);
+    this.list();
+
+    this.subs.add(
+      this.sb.rolesData$.subscribe((roles) => {
+        this.items.set(roles ?? []);
+      })
+    );
+
+    // runInInjectionContext(this.injector, () => {
+    //   effect(() => {
+    //     console.log('🔄 Criteria updated →', this.criteria?.queryString?.());
+    //     // this.list();
+    //     // this.appendSearchCriteriaToUrl();
+    //   });
+    // });
+
+    // 🔄 Emmit value change
+    runInInjectionContext(this.injector, () => {
+      effect(() => {
+        const current = this.value();
+        this.valueChange.emit(current);
+        this.onChange(current);
+      });
+    });
+  }
+
+  initCriteria(): RequestCriteria {
+    return (this.criteria = this.criteriaFactory.create());
   }
 
   list(): void {
-    this.sb.fetchRoles(this.criteria.toHttpParams());
+    this.sb.fetchRoles(this.criteria.httpParams());
+  }
+
+  override equals(a: Role, b: Role) {
+    return a.id === b.id;
+  }
+
+  override getOptionLabel(role: Role) {
+    return `${role.name} (${role.displayName})`;
   }
 }
